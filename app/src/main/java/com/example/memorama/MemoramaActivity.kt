a1598ce1b61c07f96efe6018a9a249f975385fa8
PenaPC <luis_alejandro241096@hotmail.com>
2019-03-15 00:47:01 -0600

    MEMORAMA

--- app/src/main/java/com/example/memorama/MemoramaActivity.kt
+++ app/src/main/java/com/example/memorama/MemoramaActivity.kt
@@ -1,13 +1,16 @@
 package com.example.memorama
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v7.widget.GridLayoutManager
 import com.example.games.R
 import kotlinx.android.synthetic.main.activity_memorama2.*
 
 class MemoramaActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memorama2)
@@ -15,11 +18,116 @@ class MemoramaActivity : AppCompatActivity() {
         rv.setHasFixedSize(true)
         var grid = GridLayoutManager(this, 4)
         rv.layoutManager = grid
-
+        val memoramaKeys:HashMap<Int,String> = HashMap<Int,String>()
+        val used:HashMap<String,Int> = HashMap<String,Int>()
+        used.put("batman",2)
+        used.put("bobesponja",2)
+        used.put("gofast",2)
+        used.put("kirby",2)
+        used.put("marciano",2)
+        used.put("polite",2)
+        used.put("rocket",2)
+        used.put("cohete",2)
+        used.put("space",2)
         val chips = ArrayList<Chip>()
-        for( i  in 0..12)
-            chips.add(Chip(R.mipmap.ic_launcher))
-        var  adapter = MemoramaAdapter(chips)
+        var i=0
+        loop@ while( i  < 16) {
+            val rnd = (0..7).random()
+
+            when(rnd){
+                0->{
+                    val temp=used["batman"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("batman",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "batman"))
+                    memoramaKeys[i] = "batman"
+                }
+                1->{
+                    val temp=used["bobesponja"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("bobesponja",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "bobesponja"))
+                    memoramaKeys[i] = "bobesponja"
+                }
+                2->{
+                    val temp=used["gofast"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("gofast",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "gofast"))
+                    memoramaKeys[i] = "gofast"
+                }
+                3->{
+                    val temp=used["kirby"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("kirby",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "kirby"))
+                    memoramaKeys[i] = "kirby"
+                }
+                4->{
+                    val temp=used["marciano"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("marciano",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "marciano"))
+                    memoramaKeys[i] = "marciano"
+                }
+                5->{
+                    val temp=used["polite"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("polite",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "polite"))
+                    memoramaKeys[i] = "polite"
+                }
+                6->{
+                    val temp=used["rocket"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("rocket",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "rocket"))
+                    memoramaKeys[i] = "rocket"
+                }
+                7 ->{
+                    val temp=used["space"]
+                    if(temp==0){
+                        continue@loop
+                    }
+                    if (temp != null) {
+                        used.replace("space",temp-1)
+                    }
+                    chips.add(Chip(R.mipmap.ic_launcher, i, "space"))
+                    memoramaKeys[i] = "space"
+                }
+            }
+            i++
+        }
+
+        var  adapter = MemoramaAdapter(chips, memoramaKeys)
         rv.adapter = adapter
 
     }
--- app/src/main/java/com/example/memorama/MemoramaAdapter.kt
+++ app/src/main/java/com/example/memorama/MemoramaAdapter.kt
@@ -5,32 +5,119 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
+import android.os.Handler
+import android.widget.TextView
 import com.example.games.R
 import kotlinx.android.synthetic.main.renglon.view.*
 
-class MemoramaAdapter(val chips: ArrayList<Chip>):
+class MemoramaAdapter(val chips: ArrayList<Chip>, val keys: HashMap<Int,String>):
+
 
     RecyclerView.Adapter<MemoramaAdapter.ChipViewHolder>(){
+
+    private lateinit var mHandler: Handler
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ChipViewHolder {
         val view = LayoutInflater.from(p0.context).inflate(R.layout.renglon, p0, false)
+        mHandler = Handler()
         return ChipViewHolder(view)
+
     }
 
+
+    var pushedButtons: MutableList<Int> = mutableListOf<Int>()
+    var score=0
     override fun getItemCount(): Int {
         return  chips.size
     }
 
     override fun onBindViewHolder(p0: ChipViewHolder, p1: Int) {
         p0.imageView.setImageResource(chips[p1].idImage)
+        p0.imageView.id=(chips[p1].pos)
     }
 
+    var select1: ImageView? = null
+    var select2: ImageView? = null
+
+    fun playIntent(item: ImageView){
+
+        if(select1==null){
+            select1=item
+        }else if(select2==null){
+            select2=item
+        }
+
+
+    }
+
+    fun checkPair(item:View) {
+        if(select1!=null &&select2 != null){
+            if(keys[select1!!.id] == keys[select2!!.id]){
+                score++
+                println(score)
+                pushedButtons.add(select2!!.id)
+                pushedButtons.add(select1!!.id)
+                select1 = null
+                select2 = null
+                if(score==8){
+                    Toast.makeText(item.context,
+                        "GANASTE", Toast.LENGTH_LONG).show()
+                }
+            }else {
+                Handler().postDelayed({
+                    select1!!.setImageResource(R.mipmap.ic_launcher)
+                    select2!!.setImageResource(R.mipmap.ic_launcher)
+                    select1 = null
+                    select2 = null
+                },500)
+            }
+
+        }
+
+    }
 
     inner class ChipViewHolder(item : View) : RecyclerView.ViewHolder(item){
         val imageView = item.findViewById<ImageView>(R.id.chip)
 
         init {
             item.setOnClickListener {
-                imageView.setImageResource(R.drawable.rocket)
+                if(imageView.id !in pushedButtons) {
+                    when (keys[imageView.id]) {
+                        "batman" -> {
+                            imageView.setImageResource(R.drawable.batman)
+                            playIntent(imageView)
+                        }
+                        "bobesponja" -> {
+                            imageView.setImageResource(R.drawable.bobsponja)
+                            playIntent(imageView)
+                        }
+                        "gofast" -> {
+                            imageView.setImageResource(R.drawable.gofast)
+                            playIntent(imageView)
+                        }
+                        "kirby" -> {
+                            imageView.setImageResource(R.drawable.kirby)
+                            playIntent(imageView)
+                        }
+                        "marciano" -> {
+                            imageView.setImageResource(R.drawable.marciano)
+                            playIntent(imageView)
+                        }
+                        "polite" -> {
+                            imageView.setImageResource(R.drawable.polite)
+                            playIntent(imageView)
+                        }
+                        "rocket" -> {
+                            imageView.setImageResource(R.drawable.rocket)
+                            playIntent(imageView)
+                        }
+                        "space" -> {
+                            imageView.setImageResource(R.drawable.space)
+                            playIntent(imageView)
+                        }
+                    }
+                }
+                checkPair(item)
             }
         }
     }
